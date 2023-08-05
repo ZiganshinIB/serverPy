b2ea8e83a5a4e36cab8367ad7c1da1d7e2a2e191
ZiganshinIB <elzig3012@gmail.com>
2023-08-05 17:10:16 +0300

    Add users in DB

--- MySql_db/MySqlDB.py
+++ MySql_db/MySqlDB.py
@@ -22,7 +22,7 @@ class MySqlDB:
     def create_table(self, name, column: dict):
         try:
             with self.connection.cursor() as cursor:
-                #create_table_query = f"DROP TABLE IF EXISTS `{name}`;"
+                # create_table_query = f"DROP TABLE IF EXISTS `{name}`;"
                 create_table_query = f"CREATE TABLE `{name}`(\n\tid int AUTO_INCREMENT,\n"
                 for c in column:
                     create_table_query += f"\t{c} {column[c]},\n"
@@ -40,19 +40,43 @@ class MySqlDB:
     def insert_data_in_tabel(self, table_name, datas: list[dict]):
         try:
             with self.connection.cursor() as cursor:
-                #create_table_query = f"DROP TABLE IF EXISTS `{name}`;"
+                # create_table_query = f"DROP TABLE IF EXISTS `{name}`;"
 
+                insert_query = ''
                 for d in datas:
-                    insert_query = f"INSERT INTO `{table_name}` ("
+                    insert_query += f"INSERT INTO `{table_name}` ("
                     insert_data = ''
                     for col in d:
                         insert_query += f"{col}, "
                         temp = d[col]
                         insert_data += f"{temp}, " if (type(temp) == int or type(temp) == float) else f"'{temp}', "
-                    insert_query = insert_query[:-2] + ") VALUES(" + insert_data[:-2] + ");"
+                    insert_query = insert_query[:-2] + ") VALUES(" + insert_data[:-2] + ");\n"
                 print(insert_query)
                 cursor.execute(insert_query)
-                self.connection.connect()
+                self.connection.commit()
+        except Exception as ex:
+            print("Error...")
+            print(ex)
+        finally:
+            self.connection.close()
+
+    def insert_datas_in_tabel(self, table_name, col: list, datas: list):
+        try:
+            with self.connection.cursor() as cursor:
+                # create_table_query = f"DROP TABLE IF EXISTS `{name}`;"
+
+                insert_query = ''
+                insert_query += f"INSERT INTO `{table_name}` ({', '.join(col)}) VALUES\n"
+                insert_datas = ''
+                for col_d in datas:
+                    insert_datas += '('
+                    for data in col_d:
+                        insert_datas += f"{data}, " if (type(data) == int or type(data) == float) else f"'{data}', "
+                    insert_datas =insert_datas[:-2] +'),\n'
+                insert_query = insert_query + insert_datas[:-2] + ";"
+                print(insert_query)
+                cursor.execute(insert_query)
+                self.connection.commit()
         except Exception as ex:
             print("Error...")
             print(ex)
@@ -60,3 +84,18 @@ class MySqlDB:
             self.connection.close()
 
 
+def update(self, table_name, ref: dict, filter_):
+    try:
+        with self.connection.cursor() as cursor:
+            update_query = f"UPDATE `{table_name}` SET "
+            for d in ref:
+                temp = ref[d]
+                update_query += f"{d} ="
+                update_query += f"{temp}, " if (type(temp) == int or type(temp) == float) else f"'{temp}', "
+            update_query = update_query[:-2] + "WHERE " + filter_ + ";"
+            print(update_query)
+    except Exception as ex:
+        print("Error...")
+        print(ex)
+    finally:
+        self.connection.close()
--- MySql_db/test.py
+++ MySql_db/test.py
@@ -11,11 +11,22 @@ table_name = "users_test"
 # }
 # db.create_table(table_name, columns)
 
-data = [
-    {
-        'name': "ilmir",
-        'password': '34134',
-        'email': 'iiafd'
-    }
+# data = [
+#     {
+#         'name': "ilmir",
+#         'password': '3dsafd4134',
+#         'email': 'iiafdadafd'
+#     },
+#
+# ]
+# db.insert_data_in_tabel(table_name,data)
+
+col = ['name', 'password', 'email']
+datas = [
+    ['Artur', '234eefewqer', 'fda543' ],
+    ['Artur', 'ewqer', 'fda543' ],
+    ['Ilmir', '234adfr', 'fdada543' ]
 ]
-db.insert_data_in_tabel(table_name,data)
+db.insert_datas_in_tabel(table_name, col, datas)
+
+
